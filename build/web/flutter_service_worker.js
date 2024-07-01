7107e3f799d7d53bca8f9ff4b3a09acb0a235663
Jose Aguilar <josebernardoaguilar@gmail.com>
2024-07-01 08:53:42 -0700

    carga al servidor

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "bda69b2d94259a62c8374769b8373c49
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6f4e725575a7cb3a3e149119321e5b5c",
-"/": "6f4e725575a7cb3a3e149119321e5b5c",
-"main.dart.js": "ae2d98f2db2d813573cc3dc7b01b6f15",
+"index.html": "4de2e5d45ecafbb5efdd83b21a4709a2",
+"/": "4de2e5d45ecafbb5efdd83b21a4709a2",
+"main.dart.js": "03681c901de0464a3549b5336ca6c4f4",
 "manifest.json": "f114c3de09afa176687a69e91ea009c1",
 "version.json": "7e475e96dd6d65aec55085d2d073c8d3"};
 // The application shell files that are downloaded before a service worker can
